2339f93d074c46a7c870d4bc4f8cb30cbe1e905b
Dushyant Singh Shekhawat <dushyant225@gmail.com>
2016-06-19 06:16:10 +0530

    Fixed CheatActivity rotation issue

--- app/src/main/java/dushyant/android/geoquiz/CheatActivity.java
+++ app/src/main/java/dushyant/android/geoquiz/CheatActivity.java
@@ -15,7 +15,10 @@ public class CheatActivity extends AppCompatActivity {
     private static final String EXTRA_ANSWER_SHOWN =
             "dushyant.android.geoquiz.answer_shown";
 
+    private static final String KEY_ANSWER_SHOWN = "answer_shown";
+
     private boolean mAnswerIsTrue;
+    private boolean mAnswerShown;
 
     private TextView mAnswerTextView;
     private Button mShowAnswer;
@@ -36,6 +39,10 @@ public class CheatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cheat);
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        if (savedInstanceState != null) {
+            mAnswerShown = savedInstanceState.getBoolean(KEY_ANSWER_SHOWN);
+            setAnswerShownResult();
+        }
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
 
@@ -45,14 +52,21 @@ public class CheatActivity extends AppCompatActivity {
             public void onClick(View v) {
                 int resId = (mAnswerIsTrue) ? R.string.true_button : R.string.false_button;
                 mAnswerTextView.setText(resId);
-                setAnswerShownResult(true);
+                mAnswerShown = true;
+                setAnswerShownResult();
             }
         });
     }
 
-    private void setAnswerShownResult(boolean isAnswerShown) {
+    private void setAnswerShownResult() {
         Intent data = new Intent();
-        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        data.putExtra(EXTRA_ANSWER_SHOWN, mAnswerShown);
         setResult(RESULT_OK, data);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_ANSWER_SHOWN, mAnswerShown);
     }
 }
